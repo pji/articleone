4334b32d4513355fbf1ee004fe6e184fbfad67c4
Paul Iutzi <pji@mac.com>
2019-10-16 17:56:53 -0500

    Implemented cli.Status.

--- articleone/cli.py
+++ articleone/cli.py
@@ -4,9 +4,55 @@ cli
 
 A module for running articleone's command line interface.
 """
+import sys
+import time
 
 
+# Utility classes.
 class Status:
-    """A terminal status updater."""
-    def __init__(self, title):
+    """A simple terminal status updater."""
+    def __init__(self, title:str, prefix:str):
         self.title = title
+        self.prefix = prefix
+        self.msg = None
+        self.t0 = time.time()
+    
+    def end(self):
+        """Announce the end of the status updates."""
+        sys.stdout.write('\n')
+        duration = time.time() - self.t0
+        tmp = ''.join([self.prefix, 'End'])
+        sys.stdout.write(tmp.format(duration))
+        sys.stdout.write('\n')
+        sys.stdout.write('-' * len(self.title))
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+    
+    def start(self):
+        """Announce the start of the monitored process."""
+        print(self.title.upper())
+        print('-' * len(self.title))
+    
+    def set(self, tmp:str, *args):
+        """Set a new update message and perform an initial update."""
+        sys.stdout.write('\n')
+        duration = time.time() - self.t0
+        self.tmp = ''.join([self.prefix, tmp])
+        self.msg = self.tmp.format(duration, *args)
+        sys.stdout.write(self.msg)
+        sys.stdout.flush()
+        
+    def update(self, *args):
+        """Update the status message."""
+        duration = time.time() - self.t0
+        new_msg = self.tmp.format(duration, *args)
+        sys.stdout.write('\x08' * len(self.msg))
+        sys.stdout.write(new_msg)
+        sys.stdout.flush()
+        self.msg = new_msg
+
+
+# Command scripts.
+def senators():
+    """Output a list of U.S. senators."""
+    pass
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,11 +4,28 @@ test_cli
 
 This module contains the tests for the articleone.cli module.
 """
+from contextlib import contextmanager
+from io import StringIO
+import sys
 import unittest
+from unittest.mock import patch
 
 from articleone import cli
 
 
+# Utility functions.
+@contextmanager
+def capture():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+# Tests.
 class StatusTestCase(unittest.TestCase):
     def test__init(self):
         """cli.Status: The cli.Status class should populate its 
@@ -16,11 +33,94 @@ class StatusTestCase(unittest.TestCase):
         """
         expected = [
             'Spam',
+            '{:>7.2} ',
         ]
         
         status = cli.Status(*expected)
         actual = [
             status.title,
+            status.prefix,
         ]
         
         self.assertEqual(expected, actual)
+    
+    def test__start(self):
+        """cli.Status.start: The method should announce the 
+        start of the activity.
+        """
+        expected = ('SPAM\n'
+                    '----\n')
+        expected_prefix = '{:>7.2} '
+        
+        status = cli.Status('spam', '{:>7.2} ')
+        with capture() as (out, err):
+            status.start()
+        actual = out.getvalue()
+        
+        self.assertEqual(expected, actual)
+    
+    @patch('time.time')
+    def test__end(self, mock__time):
+        """cli.Status.end: The method should announce the end 
+        of the activity.
+        """
+        expected = '\n{:7.2f} End\n----\n'.format(0.00)
+        
+        mock__time.return_value = 0.00
+        status = cli.Status('spam', '{:>7.2f} ')
+        with capture() as (out, err):
+            status.end()
+        actual = out.getvalue()
+        
+        self.assertEqual(expected, actual)
+    
+    @patch('time.time')
+    def test__set(self, mock__time):
+        """cli.Status.set: Given a message template and initial 
+        values, the method should set the new message, and 
+        print an initial update.
+        """
+        expected = '\n{:7.2f} Eggs 0'.format(0.00)
+        
+        mock__time.return_value = 0.00
+        tmp = 'Eggs {}'
+        status = cli.Status('spam', '{:>7.2f} ')
+        with capture() as (out, err):
+            status.set(tmp, 0)
+        actual_out = out.getvalue()
+        actual_msg = status.msg
+        
+        self.assertEqual(expected, actual_out)
+        self.assertEqual(expected[1:], actual_msg)
+    
+    @patch('time.time')
+    def test__update(self, mock__time):
+        """cli.Status.update: Given an update to the status, 
+        the method should remove the initial status and write 
+        an updated status.
+        """
+        expected_msg = '{:7.2f} Eggs 2'.format(1.00)
+        clear = '\x08' * len(expected_msg)
+        expected_out = clear + expected_msg
+        
+        mock__time.return_value = 1.00
+        status = cli.Status('spam', '{:>7.2f} ')
+        status.tmp = '{:7.2f} Eggs {}'
+        status.msg = '{:7.2f} Eggs 1'.format(1.00)
+        mock__time.return_value = 2.00
+        with capture() as (out, err):
+            status.update(2)
+        actual_out = out.getvalue()
+        actual_msg = status.msg
+        
+        self.assertEqual(expected_out, actual_out)
+        self.assertEqual(expected_msg, actual_msg)
+
+
+@unittest.skip
+class SenatorsTestCase(unittest.TestCase):
+    def test_valid(self):
+        """cli.senators: The function should output a list of 
+        U.S. senators.
+        """
+        raise NotImplementedError
